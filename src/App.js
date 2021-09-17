1d7447d5680b891a695cb3b0472655ebde511be4
Logan Roth <76756897+Lo-GR@users.noreply.github.com>
2021-09-17 15:18:15 -0700

    add initial three js set up

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import logo from './logo.svg';
+import * as THREE from "three"
 import './App.css';
 
 function App() {
+  const canvas = document.querySelector('canvas.webgl');
+  const scene = new THREE.Scene();
+  const geometry = new THREE.BoxGeometry(1, 1, 1);
+  const material = new THREE.MeshStandardMaterial();
+  material.color = new THREE.Color(0xff000);
+
   return (
     <div className="App">
       <header className="App-header">
+      <canvas className="webgl"></canvas>
         {/* <img src={logo} className="App-logo" alt="logo" /> */}
       </header>
     </div>
