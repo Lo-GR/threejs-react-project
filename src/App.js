def1b142a5cb0a63d665cfa3f060ace7628e444c
Logan Roth <76756897+Lo-GR@users.noreply.github.com>
2021-09-17 15:19:44 -0700

    add basic point light

--- src/App.js
+++ src/App.js
@@ -10,6 +10,15 @@ function App() {
   material.color = new THREE.Color(0xff000);
   const square = new THREE.Mesh(geometry,material)
   scene.add(square)
+
+  //Basic pointlight
+  const pointLight = new THREE.PointLight(0xffffff, 0.1)
+  pointLight.position.x = 2
+  pointLight.position.y = 3
+  pointLight.position.z = 4
+  scene.add(pointLight)
+
+  
   return (
     <div className="App">
       <header className="App-header">
