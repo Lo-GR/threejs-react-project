803c9b8716d16fcf27be18a9fc0c41ee4a8114d1
Logan Roth <76756897+Lo-GR@users.noreply.github.com>
2021-09-20 13:46:16 -0700

    create basic scene set up

--- src/FiberThree.js
+++ src/FiberThree.js
@@ -1,10 +1,18 @@
 import React from 'react'
 import "./FiberThree.css"
+import { Canvas } from '@react-three/fiber'
 
 export default function FiberThree() {
   return (
     <div className="three-container">
-      
+      <Canvas>
+        <ambientLight intensity={.1} />
+        <directionalLight color="red" position={[0,0,5]} />
+        <mesh>
+          <boxGeometry args={[2, 2, 2]}/>
+          <meshStandardMaterial />
+        </mesh>
+      </Canvas>
     </div>
   )
 }
