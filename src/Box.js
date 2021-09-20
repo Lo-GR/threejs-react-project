d8cee1a56c8011ccb46cfe96086348cf96fb0b6c
Logan Roth <76756897+Lo-GR@users.noreply.github.com>
2021-09-20 14:43:00 -0700

    add click boxes example

--- src/Box.js
+++ src/Box.js
@@ -2,14 +2,22 @@ import React, {useRef, useState} from 'react'
 import "./FiberThree.css"
 import { Canvas, useFrame } from '@react-three/fiber'
 
-export default function Box() {
+export default function Box({position}) {
   const mesh = useRef()
   const [hovered, setHover] = useState(false);
   const [active, setActive] = useState(false);
   useFrame((state, delta) => (mesh.current.rotation.x += 0.01))
   return (
-    <div>
-      
-    </div>
+    <mesh
+      position={position}
+      ref={mesh}
+      scale={active ? 1.5 : 1}
+      onClick={(e => setActive(!active))}
+      onPointerOver = {e => setHover(true)}
+      onPointerOut = {e => setHover(false)}
+    >
+      <boxGeometry args = {[1, 1, 1]} />
+      <meshStandardMaterial color={hovered ? "hotpink" : "orange"} />
+    </mesh>
   )
 }
--- src/FiberThree.js
+++ src/FiberThree.js
@@ -1,17 +1,15 @@
-import React, {useRef, useState} from 'react'
+import React from 'react'
 import "./FiberThree.css"
-import { Canvas, useFrame } from '@react-three/fiber'
+import { Canvas } from '@react-three/fiber'
+import Box from './Box'
 
 export default function FiberThree() {
   return (
     <div className="three-container">
       <Canvas>
         <ambientLight />
-        <directionalLight color="red" position={[1,1,5]} />
-        <mesh>
-          <sphereGeometry args={[2, 20, 20]}/>
-          <meshStandardMaterial />
-        </mesh>
+        <Box position={[-1.2, 0, 0]}/>
+        <Box position={[1.2, 0, 0]} />
       </Canvas>
     </div>
   )
