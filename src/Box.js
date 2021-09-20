68d9ca88a9d9da3c1665192ec21b4f7a95edb862
Logan Roth <76756897+Lo-GR@users.noreply.github.com>
2021-09-20 15:59:34 -0700

    WIP getting textureLoader working

--- src/Box.js
+++ src/Box.js
@@ -1,23 +1,33 @@
 import React, {useRef, useState} from 'react'
 import "./FiberThree.css"
-import { Canvas, useFrame } from '@react-three/fiber'
+import { TextureLoader } from 'three'
+import { Canvas, useFrame, useLoader } from '@react-three/fiber'
 
 export default function Box({position}) {
+  const textureLoader = new TextureLoader();
+  const normalMap = textureLoader.load('../public/NormalMap.png')
   const mesh = useRef()
   const [hovered, setHover] = useState(false);
   const [active, setActive] = useState(false);
   useFrame((state, delta) => (mesh.current.rotation.x += 0.01))
+  // const [ normalMap ] = useLoader(TextureLoader, ['/textures/NormalMap.png'])
   return (
     <mesh
       position={position}
       ref={mesh}
       scale={active ? 1.5 : 1}
+      
       onClick={(e => setActive(!active))}
       onPointerOver = {e => setHover(true)}
       onPointerOut = {e => setHover(false)}
     >
       <boxGeometry args = {[1, 1, 1]} />
-      <meshStandardMaterial color={hovered ? "hotpink" : "orange"} />
+      <meshStandardMaterial 
+        color={hovered ? "hotpink" : "orange"} 
+        metalness={.7}
+        roughness={.2}
+        normalMap={normalMap}
+      />
     </mesh>
   )
 }
--- src/FiberThree.js
+++ src/FiberThree.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {Suspense} from 'react'
 import "./FiberThree.css"
 import { Canvas } from '@react-three/fiber'
 import Box from './Box'
@@ -7,9 +7,11 @@ export default function FiberThree() {
   return (
     <div className="three-container">
       <Canvas>
-        <ambientLight />
-        <Box position={[-1.2, 0, 0]}/>
-        <Box position={[1.2, 0, 0]} />
+
+          <ambientLight />
+          <Box position={[-1.2, 0, 0]}/>
+          <Box position={[1.2, 0, 0]} />
+
       </Canvas>
     </div>
   )
